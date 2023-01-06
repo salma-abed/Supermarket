4fc8d4d344d2ffe50fca25a8c4d28bb8306245a4
Salma Abed <salma1910328@miuegypt.edu.eg>
2023-01-06 18:46:12 +0200

    test

--- supermarket/test/widget_test.dart
+++ supermarket/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:supermarket/main.dart';
+import 'package:mobileproject/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
