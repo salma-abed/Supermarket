9cca1bb7b808844300f9f977cb999efca4ffc347
Salma Abed <salma1910328@miuegypt.edu.eg>
2023-01-07 19:39:48 +0200

    eagae

--- supermarket/lib/main.dart
+++ supermarket/lib/main.dart
@@ -1,33 +1,50 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: unused_import, must_be_immutable, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mobileproject/categries.dart';
 import 'package:mobileproject/dashboard.dart';
+import 'package:mobileproject/loading.dart';
+import 'package:mobileproject/login.dart';
+import 'package:mobileproject/signup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Pref.init();
+
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+class MyApp extends ConsumerWidget {
+  final _router = GoRouter(routes: [
+    GoRoute(path: "/", builder: (context, state) => LoginPage(), routes: [
+      GoRoute(
+        path: "login",
+        builder: (context, state) => const DashBoard(),
       ),
-      home: const LoadingPage(),
-    );
-  }
-}
+      GoRoute(
+        path: "Signup",
+        builder: (context, state) => SignupPage(),
+      ),
+      GoRoute(
+        path: "Categories",
+        builder: (context, state) => Category(),
+      ),
+    ]),
+  ]);
+  MyApp({super.key});
 
-class LoadingPage extends StatelessWidget {
-  const LoadingPage({super.key});
-
+  // This widget is the root of your application
+  bool dark = true;
   @override
-  Widget build(BuildContext context) {
-
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      routeInformationProvider: _router.routeInformationProvider,
+    );
   }
 }
