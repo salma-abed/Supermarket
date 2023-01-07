c1bed80b16f1fe873c2eed43cdb500aac82a581a
Salma Abed <salma1910328@miuegypt.edu.eg>
2023-01-07 18:32:49 +0200

    dashboard updates

--- supermarket/lib/dashboard.dart
+++ supermarket/lib/dashboard.dart
@@ -31,42 +31,24 @@ class _DashBoardState extends State<DashBoard> {
                 inputs(height:150,
                 margin : 20,
                 padding: 10,
-                text:"butchery",
+                text:"Butchery",
                 fontSize: 30,
                 img: AssetImage(
                           'images/butchery.jpeg',
                         ),
                 radius: 70,
                 ),
-                Container(
-                  height: 150,
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Color(0xffe0fbfc).withOpacity(0.55),
-                      border: Border.all(
-                        color: Colors.transparent,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(30))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: AssetImage(
+                inputs(height:150,
+                margin : 20,
+                padding: 10,
+                text:"Fruits And Vegatables",
+                fontSize: 30,
+                img: AssetImage(
                           'images/FruitsAndVegatables.jpeg',
                         ),
-                        radius: 70,
-                      ),
-                      Text(
-                        'Fruits And Vegatables',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      )
-                    ],
-                  ),
+                radius: 70,
                 ),
+                
                 Container(
                   height: 150,
                   margin: EdgeInsets.all(20),
