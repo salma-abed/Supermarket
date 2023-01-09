a1e5289549e04c8cc8692857a26fb3324d6867c3
Salma Abed <salma1910328@miuegypt.edu.eg>
2023-01-09 17:22:05 +0200

    firebase

--- supermarket/lib/main.dart
+++ supermarket/lib/main.dart
@@ -9,10 +9,14 @@ import 'package:mobileproject/loading.dart';
 import 'package:mobileproject/login.dart';
 import 'package:mobileproject/signup.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(ProviderScope(child: MyApp()));
 }
 
