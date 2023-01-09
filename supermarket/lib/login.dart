bdb4d219ea836277f0112ec77a96ed90efa6aa70
Salma Abed <salma1910328@miuegypt.edu.eg>
2023-01-09 20:42:30 +0200

    UI Login adjustments

--- supermarket/lib/login.dart
+++ supermarket/lib/login.dart
@@ -8,28 +8,82 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
-  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      // appBar: AppBar(title: Center(
+      //   child: Text('Login',
+      //   style: TextStyle(color: Color.fromARGB(255, 6, 11, 51),
+      //                     fontFamily: 'Poppins',
+      //                     fontSize: 30,
+      //                     fontWeight: FontWeight.w500)),
+      // ) ,
+      // backgroundColor: Color.fromARGB(255, 60, 172, 247),
+      // //centerTitle: center,
+      // ),
       body: Container(decoration: BoxDecoration(
     image: DecorationImage(
         image: AssetImage('images/background/background5.jpg'),
         fit: BoxFit.fill),
   ),
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(children: <Widget>[
-            TextField(controller: _usernameController, decoration: InputDecoration(hintText: 'Username')),
+          padding: const EdgeInsets.only(left:16.0,right:16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+      Container(
+          height: 150,
+          width: 150,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    'images/grocery-cart.png',
+                  ),
+                  fit: BoxFit.fill)),
+        ),
+        SizedBox(height: 100),
+            Container(
+              margin:EdgeInsets.only(left:20,right:20),
+              decoration:BoxDecoration(
+                color:Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(fontWeight: FontWeight.w700),
+                    contentPadding: EdgeInsets.only(left:10),
+                    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+                    borderSide:BorderSide( color: Colors.transparent) ,
+                    )
+                    ),
+                    )
+                    ),
             SizedBox(height: 8.0), // Add some space between fields. 
-
-            TextField(controller: _passwordController, obscureText: true, decoration: InputDecoration(hintText:'Password')),
-
-            SizedBox(height: 8.0), // Add some space between fields.
-
+            Container(
+              margin:EdgeInsets.only(left:20,right:20),
+              decoration:BoxDecoration(
+                color:Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                ),
+              child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText:'Password',
+                  hintStyle: TextStyle(fontWeight:FontWeight.w700),
+                  contentPadding: EdgeInsets.only(left:10),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.transparent),
+                  ),
+              ),
+              ),
+            ),
+            SizedBox(height: 20.0), // Add some space between fields.
             Container(
                     alignment: Alignment.center,
                     child: GestureDetector(
@@ -38,17 +92,17 @@ class _LoginPageState extends State<LoginPage> {
                             alignment: Alignment.center,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(15),
-                                color: Colors.transparent),
+                                color: Colors.white),
                             width: 100,
                             height: 45,
-                            child: const Text("LogIn",
+                            child: const Text("Login",
                                 style: TextStyle(
                                     color: Color(0xFF191C32),
-                                    fontFamily: 'Poppins',
                                     fontSize: 20,
                                     fontWeight: FontWeight.w500)))),
                   ),
-Container(
+                  SizedBox(height: 8.0),
+            Container(
                     alignment: Alignment.center,
                     child: GestureDetector(
                         onTap: () {context.go("/Signup");},
@@ -56,13 +110,12 @@ Container(
                             alignment: Alignment.center,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(15),
-                                color: Colors.transparent),
+                                color: Colors.white),
                             width: 100,
                             height: 45,
-                            child: const Text("Signup",
+                            child: const Text("SignUp",
                                 style: TextStyle(
                                     color: Color(0xFF191C32),
-                                    fontFamily: 'Poppins',
                                     fontSize: 20,
                                     fontWeight: FontWeight.w500)))),
                   ),
