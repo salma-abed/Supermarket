057efb66438b53d135a1fe0fa92bb8b75bf45c4c
Salma Abed <salma1910328@miuegypt.edu.eg>
2023-01-10 17:20:22 +0200

    updates

--- supermarket/lib/screens/login.dart
+++ supermarket/lib/screens/login.dart
@@ -10,34 +10,37 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('images/background/background5.jpg'),
-              fit: BoxFit.fill),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 150,
-                  width: 150,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                            'images/grocery-cart.png',
-                          ),
-                          fit: BoxFit.fill)),
-                ),
-                SizedBox(height: 100),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/background/background5.jpg'),
+                fit: BoxFit.fill),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Form(
+                key: _formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 150,
+                      width: 150,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                'images/grocery-cart.png',
+                              ),
+                              fit: BoxFit.fill)),
+                    ),
+                    SizedBox(height: 100),
                 Container(
                   margin: EdgeInsets.only(left: 20, right: 20),
                   decoration: BoxDecoration(
@@ -47,22 +50,22 @@ class _LoginPageState extends State<LoginPage> {
                   child: defaultFormField(
                       validate: (value) {
                         if (value!.isEmpty) {
-                          return 'Please Enter Your Username';
+                          return 'Please Enter a valid e-mail';
                         }
                         return null;
                       },
                       controller: _emailController,
-                      type: TextInputType.emailAddress,
+                      type: TextInputType.name,
                       prefix: Icons.email,
-                      hint: 'Email Address',
-                      hintstyle: context,
+                      hint: 'E-mail',
+                      hintstyle: TextStyle(fontWeight: FontWeight.w700),
                       contentpadding: EdgeInsets.only(left: 10),
                       focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
                         borderSide: BorderSide(color: Colors.transparent),
                       )),
                 ),
-                SizedBox(height: 8.0),
+                    SizedBox(height: 8.0),
                 Container(
                   margin: EdgeInsets.only(left: 20, right: 20),
                   decoration: BoxDecoration(
@@ -70,20 +73,26 @@ class _LoginPageState extends State<LoginPage> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: defaultFormField(
-                    controller: _passwordController,
-                    type: TextInputType.text,
-                    prefix: Icons.lock,
-                    validate: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please Enter Your Password';
-                      }
-                      return null;
-                    },
-                    hint: 'Password',
-                  ),
+                      validate: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Enter a password';
+                        }
+                        return null;
+                      },
+                      controller: _passwordController,
+                      isPassword: true,
+                      type: TextInputType.name,
+                      prefix: Icons.lock,
+                      hint: 'Password',
+                      hintstyle: TextStyle(fontWeight: FontWeight.w700),
+                      contentpadding: EdgeInsets.only(left: 10),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      )),
                 ),
-                SizedBox(height: 20.0),
-                Container(
+                    SizedBox(height: 20.0),
+            Container(
                   alignment: Alignment.center,
                   child: GestureDetector(
                       onTap: () {
@@ -93,6 +102,7 @@ class _LoginPageState extends State<LoginPage> {
                           );
                           context.go("/DashBoard");
                         }
+                        ;
                       },
                       child: Container(
                           alignment: Alignment.center,
@@ -107,40 +117,68 @@ class _LoginPageState extends State<LoginPage> {
                                   fontSize: 20,
                                   fontWeight: FontWeight.w500)))),
                 ),
-                SizedBox(height: 8.0),
-                Container(
-                  alignment: Alignment.center,
-                  child: GestureDetector(
-                      onTap: () {
-                        context.go("/Signup");
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Don\'t have an account?',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 20,
-                              color: Color(0xffee6c4d),
-                            ),
-                          ),
-                          Container(
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15),
+            
+            
+                    // Container(
+                    //   alignment: Alignment.center,
+                    //   child: GestureDetector(
+                    //       onTap: () {
+                    //         if (_formKey.currentState!.validate()) {
+                    //           ScaffoldMessenger.of(context).showSnackBar(
+                    //             const SnackBar(content: Text('Processing Data')),
+                    //           );
+                    //           context.go("/DashBoard");
+                    //         }
+                    //       },
+                    //       child: Container(
+                    //           alignment: Alignment.center,
+                    //           decoration: BoxDecoration(
+                    //               borderRadius: BorderRadius.circular(15),
+                    //               color: Colors.white),
+                    //           width: 100,
+                    //           height: 45,
+                    //           child: const Text("Login",
+                    //               style: TextStyle(
+                    //                   color: Color(0xFF191C32),
+                    //                   fontSize: 20,
+                    //                   fontWeight: FontWeight.w500)))),
+                    // ),
+                    SizedBox(height: 8.0),
+                    Container(
+                      alignment: Alignment.center,
+                      child: GestureDetector(
+                          onTap: () {
+                            context.go("/Signup");
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Don\'t have an account?',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 20,
+                                  color: Color(0xffee6c4d),
+                                ),
                               ),
-                              width: 100,
-                              height: 45,
-                              child: const Text("SignUp",
-                                  style: TextStyle(
-                                      color: Color(0xFF191C32),
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500))),
-                        ],
-                      )),
-                ),
-              ]),
+                              Container(
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  width: 100,
+                                  height: 45,
+                                  child: const Text("SignUp",
+                                      style: TextStyle(
+                                          color: Color(0xFF191C32),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500))),
+                            ],
+                          )),
+                    ),
+                  ]),
+            ),
+          ),
         ),
       ),
     );
