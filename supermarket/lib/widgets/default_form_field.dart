8fbe94a90b8e236ab8182198444ed294ec5f6c0b
Salma Abed <salma1910328@miuegypt.edu.eg>
2023-01-10 15:17:34 +0200

    form field

---
+++ supermarket/lib/widgets/default_form_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+Widget defaultFormField({
+  required TextEditingController controller,
+  required TextInputType type,
+  void Function(String)? onSubmit,
+  void Function(String)? onChange,
+  void Function()? onTap,
+  bool isPassword = false,
+  required String? Function(String?)? validate,
+  required String? label,
+  IconData? prefix,
+  IconData? suffix,
+  void Function()? suffixPressed,
+  bool isClickable = true,
+}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: type,
+      obscureText: isPassword,
+      enabled: isClickable,
+      onFieldSubmitted: onSubmit,
+      onChanged: onChange,
+      onTap: onTap,
+      validator: validate,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(
+          prefix,
+        ),
+        suffixIcon: suffix != null
+            ? IconButton(
+                onPressed: suffixPressed,
+                icon: Icon(
+                  suffix,
+                ),
+              )
+            : null,
+        border: OutlineInputBorder(),
+      ),
+    );
