c9cefb2a0809bb8f48a00f636e7c2d744d151354
salma-abed <salma1910328@miuegypt.edu.eg>
2023-01-19 18:09:43 +0200

    constants

--- supermarket/lib/constants.dart
+++ supermarket/lib/constants.dart
@@ -5,3 +5,7 @@ const EdgeInsets padding = EdgeInsets.symmetric(
   vertical: 16,
   horizontal: 16,
 );
+const kTextColor = Color(0xFF535353);
+const kTextLightColor = Color(0xFFACACAC);
+
+const kDefaultPaddin = 20.0;
