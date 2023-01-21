1d81da2832a82471549baaf402f18213439c2979
mohamed1903561 <mohamed1903561@miuegypt.edu.eg>
2023-01-21 21:55:14 +0200

    Update user.dart

--- supermarket/lib/models/user.dart
+++ supermarket/lib/models/user.dart
@@ -35,3 +35,49 @@ class usermodel {
         birthday: json['birthday'],
       );
 }
+
+// import 'dart:convert';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+// User userFromJson(String str) => User.fromJson(json.decode(str));
+
+// String userToJson(User data) => json.encode(data.toJson());
+
+// class User {
+//   User({
+//     required this.name,
+//     required this.email,
+//     required this.password,
+//     required this.phone,
+//     required this.address,
+//     required this.userid,
+//     // required this.type,
+//   });
+
+//   String name;
+//   String email;
+//   String password;
+//   String phone;
+//   String address;
+//   String userid;
+//   // String type;
+
+//   factory User.fromJson(Map<String, dynamic> json) => User(
+//         name: json["name"],
+//         email: json["email"],
+//         password: json["password"],
+//         phone: json["phone"],
+//         address: json["address"],
+//         userid: json["userid"],
+//         // type: json["type"],
+//       );
+
+//   Map<String, dynamic> toJson() => {
+//         "name": name,
+//         "email": email,
+//         "password": password,
+//         "phone": phone,
+//         "address": address,
+//         "userid": userid,
+//         // "type": type,
+//       };
+// }
