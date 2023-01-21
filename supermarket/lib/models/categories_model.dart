3bb0ee48553761899a6b43fa7a642b6100ecde15
Mostafa Ashraf <mostafa1907610@miuegypt.edu.eg>
2023-01-21 15:48:34 +0200

    nm

---
+++ supermarket/lib/models/categories_model.dart
@@ -0,0 +1,13 @@
+class CategoriesModel {
+  int? id;
+  String? name;
+  String? image;
+
+  CategoriesModel({this.id, this.name, this.image});
+
+  CategoriesModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    image = json['image'];
+  }
+}
